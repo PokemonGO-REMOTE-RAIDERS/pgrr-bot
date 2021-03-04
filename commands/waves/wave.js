2eff57223b321129ed12f0920cd68857ab72cadc
Nick Hempsey <nick@elementmarketingcompany.com>
2021-03-04 14:49:29 -0500

    Remove wave range

--- commands/waves/wave.js
+++ commands/waves/wave.js
@@ -8,9 +8,6 @@ module.exports = {
 		if (isNaN(amount)) {
 			return message.reply('that doesn\'t seem to be a valid number.');
 		}
-		else if (amount < 0 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
 
 		message.channel.send(`**✨WAVE ${amount} SENDING INVITES✨**\n\n**DON’T LEAVE WHEN THE HOST DOES.**\n_LEAVE ONLY AT 10 SECONDS IF YOU HAVE LESS PEOPLE THAN RECOMMENDED._`);
 
