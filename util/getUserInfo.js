94015b248d4c4dcc0b42d711b0e7c8a2c03c1bc3
Nick Hempsey <nick@elementmarketingcompany.com>
2021-03-08 00:01:10 -0500

    Possible fix for Auth error

--- util/getUserInfo.js
+++ util/getUserInfo.js
@@ -21,7 +21,7 @@ module.exports = async function getUserInfo(sheetIndex, user, data) {
 
 	await doc.useServiceAccountAuth({
 		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-		private_key: process.env.GOOGLE_PRIVATE_KEY,
+		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	});
 
 	await doc.loadInfo();
--- util/setUserInfo.js
+++ util/setUserInfo.js
@@ -23,7 +23,7 @@ module.exports = async function setUserInfo(sheetIndex, user, data, value) {
 
 	await doc.useServiceAccountAuth({
 		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-		private_key: process.env.GOOGLE_PRIVATE_KEY,
+		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	});
 
 	await doc.loadInfo();
