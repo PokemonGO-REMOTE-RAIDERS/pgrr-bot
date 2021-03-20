8db9e176761d707b954ff7fa606ca8f1dc29a67f
Nick Hempsey <nick@elementmarketingcompany.com>
2021-03-19 20:49:06 -0400

    Refactor %host

--- commands/waves/host.js
+++ commands/waves/host.js
@@ -11,97 +11,75 @@ module.exports = {
 		(async function() {
 			const data = 'row';
 			const user = message.author;
-			getUserInfo(process.env.sheetIndexWaveHosts, user, data)
-				.then((response) => {
-					let embed;
-					if(!response.tc || !response.ign) {
-						const tc = response.tc ? response.tc : 'not set';
-						const ign = response.ign ? response.ign : 'not set';
-						embed = {
-							title: 'Please ensure you\'ve set both IGN and TC as both are requried',
-							fields: [
-								{
-									name: 'TC',
-									value: tc,
-								},
-								{
-									name: 'IGN',
-									value: ign,
-								},
-							],
-						};
-					}
-					else {
+			const userInfo = await getUserInfo(process.env.sheetIndexWaveHosts, user, data);
 
-						embed = {
-							color: '#f1609f',
-							title: `Wavehost: ${response.ign}`,
-							description: `<@&818325677492797460> Hop on board and ride the wave with ${response.ign}`,
-							author: {
-								name: 'PokémonGO Remote Raiders',
-								icon_url: 'https://raw.githubusercontent.com/PokemonGO-REMOTE-RAIDERS/pgrr-triple-threat/main/assets/pgrr-logo.png',
-							},
-							fields: [
-								{
-									name: 'IGN',
-									value: response.ign,
-									inline: true,
-								},
-								{
-									name: 'Location',
-									value: response.location ? response.location : 'Not set',
-									inline: true,
-								},
-								{
-									name: 'Number of Hosts',
-									value: response.hosts,
-									inline: true,
-								},
-								{
-									name: 'Wave Record',
-									value: response.maxwaves,
-									inline: true,
-								},
-								{
-									name: 'Rules',
-									value: response.rules ? response.rules : 'Not set',
-									inline: false,
-								},
-							],
-							timestamp: new Date(),
-							footer: {
-								text: 'If you have questions, please tag @manager',
-							},
-						};
-					}
-					message.channel.send({ embed: embed });
+			if(!userInfo || !userInfo.tc || !userInfo.ign) {
+				return message.reply('Please set a WaveHost profile.  You can start with set ign');
+			}
 
-					if(response.tc) {
-						message.channel.send(response.tc).then((sent) => {
+			const embed = {
+				color: '#f1609f',
+				title: `Wavehost: ${userInfo.ign}`,
+				description: `<@&818325677492797460> Hop on board and ride the wave with ${userInfo.ign}`,
+				author: {
+					name: 'PokémonGO Remote Raiders',
+					icon_url: 'https://raw.githubusercontent.com/PokemonGO-REMOTE-RAIDERS/pgrr-triple-threat/main/assets/pgrr-logo.png',
+				},
+				fields: [
+					{
+						name: 'IGN',
+						value: userInfo.ign,
+						inline: true,
+					},
+					{
+						name: 'Location',
+						value: userInfo.location ? userInfo.location : 'Not set',
+						inline: true,
+					},
+					{
+						name: 'Number of Hosts',
+						value: userInfo.hosts,
+						inline: true,
+					},
+					{
+						name: 'Wave Record',
+						value: userInfo.maxwaves,
+						inline: true,
+					},
+					{
+						name: 'Rules',
+						value: userInfo.rules ? userInfo.rules : 'Not set',
+						inline: false,
+					},
+				],
+				timestamp: new Date(),
+				footer: {
+					text: 'If you have questions, please tag @manager',
+				},
+			};
 
-							const userInfos = [
-								{ data: 'tcmessageid', value: sent.id },
-								{ data: 'hosting', value: true },
-								{ data: 'hosts', value: parseInt(response.hosts) + 1 },
-								{ data: 'currentwave', value: parseInt(0) },
-							];
+			message.channel.send({ embed: embed });
 
-							setUserInfo(process.env.sheetIndexWaveHosts, user, userInfos, null);
+			message.channel.send(userInfo.tc).then((sent) => {
 
-							function deleteMsg() {
-								sent.delete();
-							}
+				const userInfos = [
+					{ data: 'tcmessageid', value: sent.id },
+					{ data: 'hosting', value: true },
+					{ data: 'hosts', value: parseInt(userInfo.hosts) + 1 },
+					{ data: 'currentwave', value: parseInt(0) },
+				];
 
-							if(response.timer > 0) {
-								setTimeout(deleteMsg, response.timer);
-							}
-						});
-					}
-				})
-				.catch((error) => {
-					console.log(error, `userid: ${user}`);
-					message.channel.send(`No data found for this user. Maybe consider using \`${process.env.prefix}set\` and add yourself!`);
-				});
+				setUserInfo(process.env.sheetIndexWaveHosts, user, userInfos, null);
+
+				function deleteMsg() {
+					sent.delete();
+				}
+
+				if(userInfo.timer > 0) {
+					setTimeout(deleteMsg, userInfo.timer);
+				}
+			});
+
 		}());
 
 	},
