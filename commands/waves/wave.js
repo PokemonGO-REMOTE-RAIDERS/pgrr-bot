dd504f3dbf25c535a45f4a6695d42a2a366ef830
Nick Hempsey <nick@elementmarketingcompany.com>
2021-04-07 12:18:26 -0400

    Minor bug fixes
    - Fix prefix errors
    -- %help
    -- %set
    -- %profile
    
    - Set bot activity to "Made by the PGRR Dev Team"
    
    - Permission changes
    -- %wave
    --- @verified users can use the base version.

--- commands/waves/wave.js
+++ commands/waves/wave.js
@@ -28,7 +28,7 @@ module.exports = {
 	expectedArgs: 1,
 	cooldown: 2,
 	noPrefix: true,
-	roles: ['roleWaveHost', 'roleAdmin'],
+	roles: ['roleWaveHost', 'roleAdmin', 'roleVerified'],
 	execute(message, args, client) {
 		(async function() {
 
